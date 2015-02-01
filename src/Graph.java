fab13a9dcebc0825905080c795d980d12984d0f7
souptikji <souptikji@gmail.com>
2015-02-01 20:40:00 +0530

    Remove worthless comments

--- src/Graph.java
+++ src/Graph.java
@@ -3,67 +3,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-
-/*************************************************************************
- *  A graph, implemented using an array of sets.
- *  Parallel edges and self-loops allowed.
- *
- *  % java Graph tinyG.txt
- *  13 vertices, 13 edges
- *  0: 6 2 1 5
- *  1: 0
- *  2: 0
- *  3: 5 4
- *  4: 5 6 3
- *  5: 3 4 0
- *  6: 0 4
- *  7: 8
- *  8: 7
- *  9: 11 10 12
- *  10: 9
- *  11: 9 12
- *  12: 11 9
- *
- *  % java Graph mediumG.txt
- *  250 vertices, 1273 edges
- *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15
- *  1: 220 203 200 194 189 164 150 130 107 72
- *  2: 141 110 108 86 79 51 42 18 14
- *  ...
- *
- *************************************************************************/
-
-/**
- *  The <tt>Graph</tt> class represents an undirected graph of vertices
- *  named 0 through <em>V</em> - 1.
- *  It supports the following two primary operations: add an edge to the graph,
- *  iterate over all of the vertices adjacent to a vertex. It also provides
- *  methods for returning the number of vertices <em>V</em> and the number
- *  of edges <em>E</em>. Parallel edges and self-loops are permitted.
- *  <p>
- *  This implementation uses an adjacency-lists representation, which
- *  is a vertex-indexed array of {@link Bag} objects.
- *  All operations take constant time (in the worst case) except
- *  iterating over the vertices adjacent to a given vertex, which takes
- *  time proportional to the number of such vertices.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/41undirected">Section 4.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- */
 public class Graph
 {
     private final int V;
     private int E;
     private List<Integer>[] adj;
 
-    /**
-     * Initializes an empty graph with <tt>V</tt> vertices and 0 edges.
-     * param V the number of vertices
-     * @throws java.lang.IllegalArgumentException if <tt>V</tt> < 0
-     */
     public Graph(int V)
     {
         if (V < 0)
@@ -82,15 +27,6 @@ public class Graph
         }
     }
 
-    /**
-     * Initializes a graph from an input stream.
-     * The format is the number of vertices <em>V</em>,
-     * followed by the number of edges <em>E</em>,
-     * followed by <em>E</em> pairs of vertices, with each entry separated by whitespace.
-     * @param in the input stream
-     * @throws java.lang.IndexOutOfBoundsException if the endpoints of any edge are not in prescribed range
-     * @throws java.lang.IllegalArgumentException if the number of vertices or edges is negative
-     */
     public Graph(InputReader in)
     {
         this(in.readInt());
