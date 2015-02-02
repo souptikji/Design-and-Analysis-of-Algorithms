57ce24db9ddd4623cb79c25cbb7f09cef4ddebd0
souptikji <souptikji@gmail.com>
2015-02-02 23:36:17 +0530

    Queue using linked list

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -27,20 +27,15 @@ import java.util.NoSuchElementException;
  *  The <em>enqueue</em>, <em>dequeue</em>, <em>peek</em>, <em>size</em>, and <em>is-empty</em>
  *  operations all take constant time in the worst case.
  *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
  */
-public class LinkedQueue<Item> implements Iterable<Item> {
+public class LinkedQueue{
     private int N;         // number of elements on queue
     private Node first;    // beginning of queue
     private Node last;     // end of queue
 
     // helper linked list class
     private class Node {
-        private Item item;
+        private int item;
         private Node next;
     }
 
@@ -75,7 +70,7 @@ public class LinkedQueue<Item> implements Iterable<Item> {
      * @return the item least recently added to this queue
      * @throws java.util.NoSuchElementException if this queue is empty
      */
-    public Item peek() {
+    public int poll() {
         if (isEmpty()) throw new NoSuchElementException("Queue underflow");
         return first.item;
     }
@@ -84,7 +79,7 @@ public class LinkedQueue<Item> implements Iterable<Item> {
      * Adds the item to this queue.
      * @param item the item to add
      */
-    public void enqueue(Item item) {
+    public void add(int item) {
         Node oldlast = last;
         last = new Node();
         last.item = item;
@@ -100,9 +95,9 @@ public class LinkedQueue<Item> implements Iterable<Item> {
      * @return the item on this queue that was least recently added
      * @throws java.util.NoSuchElementException if this queue is empty
      */
-    public Item dequeue() {
+    public int remove() {
         if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-        Item item = first.item;
+        int item = first.item;
         first = first.next;
         N--;
         if (isEmpty()) last = null;   // to avoid loitering
@@ -110,17 +105,7 @@ public class LinkedQueue<Item> implements Iterable<Item> {
         return item;
     }
 
-    /**
-     * Returns a string representation of this queue.
-     * @return the sequence of items in FIFO order, separated by spaces
-     */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (Item item : this)
-            s.append(item + " ");
-        return s.toString();
-    } 
-
+    
     // check internal invariants
     private boolean check() {
         if (N == 0) {
@@ -155,41 +140,18 @@ public class LinkedQueue<Item> implements Iterable<Item> {
         return true;
     } 
  
-
-    /**
-     * Returns an iterator that iterates over the items in this queue in FIFO order.
-     * @return an iterator that iterates over the items in this queue in FIFO order
-     */
-    public Iterator<Item> iterator()  {
-        return new ListIterator();  
-    }
-
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
-        }
-    }
-
-
     /**
      * Unit tests the <tt>LinkedQueue</tt> data type.
      */
     public static void main(String[] args) {
-        LinkedQueue<String> q = new LinkedQueue<String>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("-")) q.enqueue(item);
-            else if (!q.isEmpty()) StdOut.print(q.dequeue() + " ");
+        LinkedQueue q = new LinkedQueue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println("Hello");
+        while(!q.isEmpty())
+        {
+        	System.out.println(q.remove());
         }
-        StdOut.println("(" + q.size() + " left on queue)");
     }
 }
