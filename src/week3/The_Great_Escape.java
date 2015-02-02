c3e3a0889b8e952f23a34a97d7e8d78da2b24583
souptikji <souptikji@gmail.com>
2015-02-02 23:49:10 +0530

    back to sq1

--- src/week3/The_Great_Escape.java
+++ src/week3/The_Great_Escape.java
@@ -1,16 +1,15 @@
 package week3;
 
-
-
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.Stack;
+
 
 
 public class The_Great_Escape
@@ -361,4 +360,119 @@ class IOUtils
 
         return array;
     }
+}
+
+
+class LinkedQueue{
+    private int N;         // number of elements on queue
+    private Node first;    // beginning of queue
+    private Node last;     // end of queue
+
+    // helper linked list class
+    private class Node {
+        private int item;
+        private Node next;
+    }
+
+    /**
+     * Initializes an empty queue.
+     */
+    public LinkedQueue() {
+        first = null;
+        last  = null;
+        N = 0;
+        assert check();
+    }
+
+    /**
+     * Is this queue empty?
+     * @return true if this queue is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Returns the number of items in this queue.
+     * @return the number of items in this queue
+     */
+    public int size() {
+        return N;     
+    }
+
+    /**
+     * Returns the item least recently added to this queue.
+     * @return the item least recently added to this queue
+     * @throws java.util.NoSuchElementException if this queue is empty
+     */
+    public int poll() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return first.item;
+    }
+
+    /**
+     * Adds the item to this queue.
+     * @param item the item to add
+     */
+    public void add(int item) {
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else           oldlast.next = last;
+        N++;
+        assert check();
+    }
+
+    /**
+     * Removes and returns the item on this queue that was least recently added.
+     * @return the item on this queue that was least recently added
+     * @throws java.util.NoSuchElementException if this queue is empty
+     */
+    public int remove() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        int item = first.item;
+        first = first.next;
+        N--;
+        if (isEmpty()) last = null;   // to avoid loitering
+        assert check();
+        return item;
+    }
+
+    
+    // check internal invariants
+    private boolean check() {
+        if (N == 0) {
+            if (first != null) return false;
+            if (last  != null) return false;
+        }
+        else if (N == 1) {
+            if (first == null || last == null) return false;
+            if (first != last)                 return false;
+            if (first.next != null)            return false;
+        }
+        else {
+            if (first == last)      return false;
+            if (first.next == null) return false;
+            if (last.next  != null) return false;
+
+            // check internal consistency of instance variable N
+            int numberOfNodes = 0;
+            for (Node x = first; x != null; x = x.next) {
+               numberOfNodes++;
+            }
+            if (numberOfNodes != N) return false;
+
+            // check internal consistency of instance variable last
+            Node lastNode = first;
+            while (lastNode.next != null) {
+               lastNode = lastNode.next;
+            }
+            if (last != lastNode) return false;
+        }
+
+        return true;
+    } 
+ 
 }
